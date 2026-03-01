842b878caaabd01a69b54811b28a4409b6c9cec9
kisoritech <melijuba30@gmail.com>
2026-03-01 13:13:15 -0300

    add_info2

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '1234',
-  database: process.env.DB_NAME || 'sistema',
+  database: process.env.DB_NAME || 'josedecorando',
   waitForConnections: true,
   connectionLimit: 10,
   enableKeepAlive: true,
